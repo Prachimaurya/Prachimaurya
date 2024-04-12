0f006758015e1cf3e46b64494af7d86b2cb150b7
Prachimaurya <157804768+Prachimaurya@users.noreply.github.com>
2024-04-12 11:19:40 +0000

    container config

---
+++ basic/test.js
@@ -0,0 +1,9 @@
+console.log("prachi")
+let score = 67;
+console.log(score);
+const scores = 76;
+console.log(scores)
+let value = 20 + 10;
+let result = String(value);
+console.log(result); // prints ‘30’
+console.log(typeof result); // prints string
