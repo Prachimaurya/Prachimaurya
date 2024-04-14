4c446cf18da87abb7436a583c8c975179ae21b2b
Prachimaurya <157804768+Prachimaurya@users.noreply.github.com>
2024-04-14 12:23:32 +0000

    code

--- basic/test.js
+++ basic/test.js
@@ -17,4 +17,85 @@ city = "jaipur";
 
 console.log([accountEmail,accountpassword,city])
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++datatype+++++++++++++++++++++++++++++++++++++++++++++++++++
+let score = 5667
+let score1 = "676556"
+let score2 = "46456gghhg"
+
+// console.log(typeof [score,score1]);
+// console.log(typeof score); 
+// console.log(typeof score1);
+// console.log(typeof score2);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++conversion+++++++++++++++++++++++++++++++++
+// let value= Number(score)//number
+// let valueNumber = String(score1) //String 
+// let value2 = Number(score2)
+
+// console.log(typeof valueNumber);
+// console.log(typeof value1);
+//  console.log(typeof value2);
+
+// let login = "1";
+// console.log(login);
+// let loginn = Boolean(login)
+// console.log(typeof loginn);
+// +++++++++++++++++++++++++++++++++++++++++++++++++Arthmatic++++++++++++++++++++++++++++++++++++
+// let a = 16
+// let b = 90
+// let c= a+b
+// console.log(c);
+// console.log(556+86);
+// console.log(556**86);
+// console.log(556*86);
+// console.log(556-86);
+// console.log(5565%86);
+// 
+// ++++++++++++++++++++++
+// let game = 56
+// ++ game;//prefix
+// console.log(game); 
+// let game1 = 569
+//  game1 ++;//postfix
+// console.log(game1); 
+
+// let gam = 569
+//  gam --;
+// console.log(gam); 
+
+// primitive 
+// 7 tpyes of primitive 
+// String ,boolean ,number ,null ,undefined, symbol , BigInt
+
+// Refrenve (Non primitive) 
+
+// object . array , function
+//++++++++++++++++++++++++++++++++++++++++++++++++++++Symbol ++++++++++++++++++++++++++++++++++++++++
+//const log1 = Symbol('123')
+//const log = Symbol('123')
+//console.log(log===log1);
+// ++++++++++++++++++++++++++++++++++++++++++++++Array++++++++++++++++++
+const arr =["india", "goa" , "up" , "gujrat" , "lucknow"]
+console.log(arr);
+// +++++++++++++++++++++++++++++++++Object+++++++++++++++++++++
+let myobject = {
+    name:"prachimaurya",
+    course:"MCA",
+    age:56,
+
+    
+};
+console.log(myobject);
+
+let obj = {
+    name: "John",
+     age: 30};
+console.log(obj);
+// +++++++++++++++++++++++++++++++++++++Function ++++++++++++++++++++++++++++++
+let myFunction = function(){
+    console.log("hello world");
+
+};
+console.log(typeof [myFunction]);
 
